ea40970f86f620c77f63679b33bf2364a03b9a8f
BBR4 <joshruber@gmail.com>
2025-02-20 14:26:30 -0700

    lol

---
+++ Forms-Project/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Configure the database settings
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Josrub123@localhost/stox'  # MySQL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+app.config['SECRET_KEY'] = 'your-secret-key' 
+
+
+# Route for the home page.
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+# Route for the portfolio page.
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+# Route for the about page.
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Route for the contact page.
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Route for the admin panel.
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
